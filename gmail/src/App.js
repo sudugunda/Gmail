db4232e0f5bbc12de2eff9b8940ee7f30d9063fc
Sudu <49281434+sudugunda@users.noreply.github.com>
2021-12-07 10:05:42 +0530

    initial commit

---
+++ gmail/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Lets build gmail</h1>
+        <Header />
+    </div>
+  );
+}
+
+export default App;
